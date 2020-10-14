2669722c4f97c678351066b62f984a0ee1482161
Carlos <carlosalbertoml@ufps.edu.co>
2020-10-13 19:26:00 -0500

    vistas admin e instaFeed

--- js/main.js
+++ js/main.js
@@ -1,40 +1,13 @@
-$(window).on('load', function(){
-  $.instagramFeed({
-    'username': 'xiaomi.colombia',
-    'container': "#instagram-feed-demo"
-  });
-});
 
-$(window).on('load', function(){
-  $.instagramFeed({
-    'username': 'xiaomi.colombia',
-    'container': "#instagram-feed-demo",
-    'display_profile': true,
-    'display_biography': true,
-    'display_igtv': false
+$(document).ready(function(){
+  var insta = new InstaFeed({
+    get:'user',
+    userId:'5727217189',
+    limit:12,
+    resolution:'standard_resolution',
+    accesToken:'',
+    sortBy: 'most-recent'
+    template:'<div class="col-lg-3 gallery instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
   });
-});
-
-$(window).on('load', function(){
-  $.instagramFeed({
-    'username': 'xiaomi.colombia',
-    'container': "#instagram-feed-demo",
-    'items': 8
-  });
-});
-
-$(window).on('load', function(){
-  $.instagramFeed({
-    'username': 'xiaomi.colombia',
-    'container': "#instagram-feed-demo",
-    'items_per_row': 4
-  });
-});
-
-$(window).on('load', function(){
-  $.instagramFeed({
-    'username': 'xiaomi.colombia',
-    'container': "#instagram-feed-demo",
-    'margin': 0.5
-  });
+  insta.run();
 });
